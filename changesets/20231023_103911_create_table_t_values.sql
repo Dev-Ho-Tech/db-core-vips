ad2ea2198bda01525b45430aa2c5a6f2a1042e51
Lnavarrete14 <lnavarrete14@gmail.com>
2025-02-15 21:46:56 -0500

    Update 20231023_103911_create_table_t_values.sql

--- changesets/20231023_103911_create_table_t_values.sql
+++ changesets/20231023_103911_create_table_t_values.sql
@@ -3,7 +3,7 @@
 --changeset gcuentas:20231023_103911 runOnChange:true stripComments:false
 --comment: create_table_t_values
 
-create table sch_core.t_values
+create table t_values
 (
 
     value_id         integer      not null,
@@ -16,19 +16,19 @@ create table sch_core.t_values
     user_updated     varchar(50)  null
 );
 
-alter table sch_core.t_values
+alter table t_values
     add constraint pk_t_values primary key (value_id);
 
-comment on table sch_core.t_values is 'Tabla que guardara los estados o indicadores de la base de datos.';
+comment on table t_values is 'Tabla que guardara los estados o indicadores de la base de datos.';
 
-comment on column sch_core.t_values.value_id is 'identificador principal de la tabla';
-comment on column sch_core.t_values.code is 'codigo del cual se identificara el estado, indicador o contante que servira para las demas tablas';
-comment on column sch_core.t_values.description_desc is 'descripcion del registro.';
-comment on column sch_core.t_values.parent_id is 'id que agrupa los valores que puede tener una tabla.';
-comment on column sch_core.t_values.user_created is 'Usuario que creo el registro';
-comment on column sch_core.t_values.created_date is 'fecha en que se creo el registro';
-comment on column sch_core.t_values.updated_date is 'fecha en la que se modifico el registro';
-comment on column sch_core.t_values.user_updated is 'usuario que modifico el registro';
+comment on column t_values.value_id is 'identificador principal de la tabla';
+comment on column t_values.code is 'codigo del cual se identificara el estado, indicador o contante que servira para las demas tablas';
+comment on column t_values.description_desc is 'descripcion del registro.';
+comment on column t_values.parent_id is 'id que agrupa los valores que puede tener una tabla.';
+comment on column t_values.user_created is 'Usuario que creo el registro';
+comment on column t_values.created_date is 'fecha en que se creo el registro';
+comment on column t_values.updated_date is 'fecha en la que se modifico el registro';
+comment on column t_values.user_updated is 'usuario que modifico el registro';
 
 
 --rollback drop table sch_core.t_values;
