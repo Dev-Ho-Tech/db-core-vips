f12984da5ac52d8d7864a3eb2f00517235c8540a
Lnavarrete14 <lnavarrete14@gmail.com>
2025-02-25 01:57:33 -0500

    Update demo.sql

--- changesets/demo.sql
+++ changesets/demo.sql
@@ -1,7 +1,6 @@
 --liquibase formatted sql
 
---changeset lnavarrete:20250224_081513 runOnChange:true stripComments:false
+--changeset lnavarrete:20250224_081511 runOnChange:true stripComments:false
 --comment: create_table_t_incoming_mastercard
 
-alter table t_control_disputes_files_2
-add dispute_registered_number integer;
+create table t_control_disputes_files_2(id integer)
