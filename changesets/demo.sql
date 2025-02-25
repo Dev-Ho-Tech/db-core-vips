59ccd5e77c6b4e0837d2e16923072432997004d8
Lnavarrete14 <lnavarrete14@gmail.com>
2025-02-25 02:14:07 -0500

    Delete changesets/demo.sql

--- changesets/demo.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset lnavarrete:20250224_081511 runOnChange:true stripComments:false
---comment: create_table_t_incoming_mastercard
-
-create table IF NOT EXISTS t_control_disputes_files_2(id integer)
